d77dcf7e1bd665f39fd527fc2bd15d5daad56e03
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-08-21 18:35:21 +0300

    add api_script.py

---
+++ api_script.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+
+# URL API
+url = "https://jsonplaceholder.typicode.com/posts"
+
+try:
+    # Подключение к API и получение данных
+    response = requests.get(url)
+
+    # Проверка статуса ответа
+    if response.status_code == 200:
+        # Получаем данные в формате JSON
+        data = response.json()
+
+        # Сохранение данных в файл
+        with open('posts.json', 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, indent=4)
+
+        print("Данные успешно сохранены в файл posts.json.")
+    else:
+        print(f"Ошибка при подключении к API: {response.status_code}")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
