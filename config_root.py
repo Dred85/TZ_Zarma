2c7f3d552f7ce6f37b5f4f21f54b275e3e05c660
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-08-21 21:18:31 +0300

    fix  задание_1

---
+++ config_root.py
@@ -0,0 +1,6 @@
+import os
+
+# Абсолютный путь до ROOT_DIR
+ROOT_DIR = os.path.dirname(__file__)
+path_to_file_json_posts = os.path.join(ROOT_DIR, 'data', 'posts.json')
+# path_to_file_vacancies = os.path.join(ROOT_DIR, 'data', 'vacancies.json')
--- feature_1_api/api_script.py
+++ feature_1_api/api_script.py
@@ -1,25 +1,33 @@
 import requests
 import json
 
+from config_root import path_to_file_json_posts
+
 
 # URL API
 url = "https://jsonplaceholder.typicode.com/posts"
 
-try:
-    # Подключение к API и получение данных
-    response = requests.get(url)
+def main(url):
+    """данный скрипт подключается к API, получает данные и записывает их в файл"""
+    try:
+        # Подключение к API и получение данных
+        response = requests.get(url)
 
-    # Проверка статуса ответа
-    if response.status_code == 200:
-        # Получаем данные в формате JSON
-        data = response.json()
+        # Проверка статуса ответа
+        if response.status_code == 200:
+            # Получаем данные в формате JSON
+            data = response.json()
 
-        # Сохранение данных в файл
-        with open('feature_1_api/posts.json', 'w', encoding='utf-8') as json_file:
-            json.dump(data, json_file, indent=4)
+            # Сохранение данных в файл
+            with open(path_to_file_json_posts, 'w', encoding='utf-8') as json_file:
+                json.dump(data, json_file, indent=4)
 
-        print("Данные успешно сохранены в файл posts.json.")
-    else:
-        print(f"Ошибка при подключении к API: {response.status_code}")
-except Exception as e:
-    print(f"Произошла ошибка: {e}")
+            print("Данные успешно сохранены в файл posts.json.")
+        else:
+            print(f"Ошибка при подключении к API: {response.status_code}")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+
+if __name__ == "__main__":
+    main(url)
