37fe46fc7184ca80e4044f73580318ce2738c08e
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-08-21 22:07:51 +0300

    Убрал лишние файлы из проекта

--- config_root.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Абсолютный путь до ROOT_DIR
-ROOT_DIR = os.path.dirname(__file__)
-path_to_file_json_posts = os.path.join(ROOT_DIR, 'data', 'posts.json')
-# path_to_file_vacancies = os.path.join(ROOT_DIR, 'data', 'vacancies.json')
