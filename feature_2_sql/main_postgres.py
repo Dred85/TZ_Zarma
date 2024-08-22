1aa4f1e7e7ce404ff6c5775350d73531675dcb08
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-08-22 06:09:40 +0300

    PEP8

--- feature_2_sql/main_postgres.py
+++ feature_2_sql/main_postgres.py
@@ -1,6 +1,7 @@
 from feature_2_sql.utils_sql import create_db
 from feature_2_sql.sql_script import sql_query_for_selection
 
+
 # Решение задания №2 с использованием БД PostgreSQL
 def main():
     """Основные функции нашего скрипта:
@@ -8,7 +9,6 @@ def main():
         2. выполняет SQL-запрос для вывода информации о пользователях старше 30 лет"""
     create_db()
     sql_query_for_selection()
-
 
 
 if __name__ == "__main__":
